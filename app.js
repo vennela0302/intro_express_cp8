7f9b92a3770267e23c9b6f57a93278912f4de03d
vennela0302 <106340650+vennela0302@users.noreply.github.com>
2024-06-06 21:49:41 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const app = express();
+app.use(express.json());
+const dbPath = path.join(__dirname, "todoApplication.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+app.get("/todos/", async (req, res) => {
+  const { status } = req.query;
+  const todoQuery = `
+    SELECT * FROM todo WHERE status LIKE '%${status}%' `;
+  const getTodoStatus = await db.all(todoQuery);
+  res.send(getTodoStatus);
+});
